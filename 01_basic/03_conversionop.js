bcf718e70fe037881888603233a67566490e642a
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-06-19 07:50:06 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionop.js
@@ -0,0 +1,28 @@
+const score = "33adf"
+
+// console.log(typeof(score))
+// conversion 
+
+const convertedNumber = Number(score)
+// console.log(convertedNumber)
+// type showing number But out showes NaN
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+// converting anything to boolean
+
+let num = "ads"
+
+let isloggesin = Boolean(num)
+console.log(typeof(isloggesin));
+console.log(isloggesin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Adnan" => true
+
+
+
