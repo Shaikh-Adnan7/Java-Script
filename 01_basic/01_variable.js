14f02128d92b5f406ac7c1d35b86e306c193393b
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-06-19 06:52:22 +0000

    des

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -4,3 +4,9 @@ var city = "above the sky "
 
 
 console.table([accountID, email, city]);
+
+
+// Keyword	    Scope	    Reassignable	Hoisted	Best Use
+// var	        Function	✅ Yes	        ✅ Yes	Legacy code, avoid in modern JS
+// let	        Block	    ✅ Yes	        ❌ No	Variables that change in value
+// const	    Block	    ❌ No	        ❌ No	Constants, arrays, objects that won’t be reassigned
