8c210165b21685eb1b8254a507b1daa3b69582ec
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-06-19 07:24:50 +0000

    datatypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,8 @@
+typeof "hello"    // "string"
+typeof 42          // "number"
+typeof true        // "boolean"
+typeof undefined   // "undefined"
+typeof null        // "object" ← historical bug!
+typeof {}          // "object"
+typeof []          // "object"
+typeof function(){} // "function"
