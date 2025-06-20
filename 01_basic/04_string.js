3e588018573c6bb7284ad841edef520891a6ddcb
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-06-20 09:55:13 +0000

    string method (split , replace , trim etc )

--- 01_basic/04_string.js
+++ 01_basic/04_string.js
@@ -14,5 +14,12 @@ const gameCounter = new String("another string");
 // console.log(gameCounter.length); // Accessing the length property of the String object directly
 // console.log(gameCounter.toUpperCase()); // Calling the toUpperCase method on the String object
 
-console.log(gameCounter.trim()); // Calling the trim method on the String object
-console.log(gameCounter)
+const anotherstring = "             trimmethod       ";
+console.log(anotherstring); // Original string with leading and trailing whitespace
+console.log(anotherstring.trim()); // Using the trim method to remove whitespace from both ends of the string
+
+const url = "https://www.example.com/path/to/resource?query=string#fragment";
+console.log(url.replace("=", "-")); // Replacing the # fragment identifier with --
+
+
+console.log(gameCounter.split("#")); // Splitting the string into an array of substrings based on spaces
