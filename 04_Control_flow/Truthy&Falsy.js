3223cd97393cc4d2158f23fccc27f3b3e287de25
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-11-19 07:21:53 +0000

    Truthy And falsy values and also have Nullish Coalescing Operator and diff between it and turnery operator as the symbol of both are same ? and ??

---
+++ 04_Control_flow/Truthy&Falsy.js
@@ -0,0 +1,45 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+// The null coalescing operator (often written as ??) is used in many programming languages (such as JavaScript, PHP, C#, Dart, etc.) to provide a default value when the left-hand side is null (and in some languages, also undefined).
+// It returns the first value that is not null.
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
