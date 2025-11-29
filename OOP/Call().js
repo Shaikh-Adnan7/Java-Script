67fa9e14c9c72ebdd92dcc7057c146e33cae3827
Shaikh Adnan <136962400+Shaikh-Adnan7@users.noreply.github.com>
2025-11-29 07:42:42 +0000

    Call()

---
+++ OOP/Call().js
@@ -0,0 +1,16 @@
+// call()
+// call() is a method available on all JavaScript functions that allows you to invoke a function immediately and explicitly set the value of this inside that function. It also lets you pass arguments one by one to the function.
+
+function UserName(name) {
+  this.name = name;
+}
+
+function User(name, email, id) {
+  UserName.call(this, name);   // 
+
+  this.email = email;
+  this.id = id;
+}
+
+const user1 = new User("Ana", "ana@example.com", 101);
+console.log(user1);
